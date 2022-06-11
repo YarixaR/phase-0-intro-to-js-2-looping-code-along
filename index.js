9edf2f49771e0904fa0d3303ca88169c18fc0fa0
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-06-11 14:36:16 -0700

    commit tree a34bca8b835c41fca72224a00b880d8caea57303

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(cards, events) {
+    const messages = [];
+for(let i = 0; i < cards.length; i++) {
+let events = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+messages.push(events)
+}
+return messages;
+}
+writeCards (cards) ;
+
+function countDown(){
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+}
